92097167c4a3d4297b4d0b4afb03d6fcd8868e0f
Alexander Bartl <alexander.bartl@tngtech.com>
2023-12-02 08:21:43 +0100

    Update template: aocd no longer provides lines and numbers

--- template.py
+++ template.py
@@ -1,4 +1,10 @@
-from aocd import data, lines, submit
+import re
+
+from aocd import data, submit
+
+lines = data.split('\n')
+numbers = [list(map(int, re.findall(r'-?\d+', line))) for line in lines]
 
 print(data)
 print(lines)
+print(numbers)
