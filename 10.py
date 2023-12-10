263afadc9cf88cc1e979f1eb146e0de0cad42514
Alexander Bartl <alexander.bartl@tngtech.com>
2023-12-10 10:40:46 +0100

    Day 10

---
+++ 10.py
@@ -0,0 +1,56 @@
+import re
+
+from aocd import data, submit
+
+lines = data.split('\n')
+
+start = [(x, y) for y, line in enumerate(lines) for x, char in enumerate(line) if char == 'S'][0]
+
+paths = ([start], [start])
+while len(set(paths[0]).intersection(set(paths[1]))) < 2:
+    for i, path in enumerate(paths):
+        x, y = path[-1]
+        candidates = []
+        if lines[y][x] == '|':
+            candidates = [(x, y - 1), (x, y + 1)]
+        elif lines[y][x] == '-':
+            candidates = [(x - 1, y), (x + 1, y)]
+        elif lines[y][x] == 'L':
+            candidates = [(x + 1, y), (x, y - 1)]
+        elif lines[y][x] == 'J':
+            candidates = [(x - 1, y), (x, y - 1)]
+        elif lines[y][x] == 'F':
+            candidates = [(x + 1, y), (x, y + 1)]
+        elif lines[y][x] == '7':
+            candidates = [(x - 1, y), (x, y + 1)]
+        elif lines[y][x] == 'S':
+            if lines[y - 1][x] in '|F7':
+                candidates.append((x, y - 1))
+            if lines[y + 1][x] in '|LJ':
+                candidates.append((x, y + 1))
+            if lines[y][x - 1] in '-LF':
+                candidates.append((x - 1, y))
+            if lines[y][x + 1] in '-J7':
+                candidates.append((x + 1, y))
+            candidates = candidates[i:i + 1]
+        path.append([c for c in candidates if c not in path][0])
+
+print(len(paths[0]) - 1)
+# submit(len(paths[0]) - 1)
+
+answer_b = 0
+inside = False
+for y in range(len(lines)):
+    for x in range(len(lines[y])):
+        if (x, y) in paths[0] or (x, y) in paths[1]:
+            if lines[y][x] in '|F7':
+                inside = not inside
+            print(lines[y][x], end='')
+        else:
+            if inside:
+                answer_b += 1
+                print('I', end='')
+            print(' ', end='')
+    print()
+
+submit(answer_b)
