1e0442b901ad881c52380b3a8d4cdbf53ae96ee7
Alexander Bartl <alexander.bartl@tngtech.com>
2023-12-13 19:29:20 +0100

    WIP: some day 12 refactorings

--- 12a.py
+++ 12a.py
@@ -1,26 +1,23 @@
 import re
 
-from aocd import data, submit
+from debug import data, submit
 
 lines = data.split('\n')
 
 
 def check_line(springs, expected):
-    actual = []
-    for i, spring in enumerate(springs):
-        if spring == '#' and (i == 0 or springs[i - 1] == '.'):
-            actual.append(1)
-        elif spring == '#' and springs[i - 1] == '#':
-            actual[-1] += 1
-        elif spring == '?':
-            if not actual:
-                return True
-            return len(actual) <= len(expected) and \
-                springs[i:].replace('?', '#').count('#') >= sum(expected[len(actual):]) and \
-                actual[:-1] == expected[:len(actual) - 1] and \
-                actual[-1] <= expected[len(actual) - 1]
+    known_springs = springs[:springs.index('?')] if '?' in springs else springs
+    actual = [len(x) for x in known_springs.split('.') if x]
 
-    return actual == expected
+    if springs == known_springs:
+        return actual == expected
+
+    if not actual:
+        return True
+
+    return len(actual) <= len(expected) and \
+        actual[:-1] == expected[:len(actual) - 1] and \
+        actual[-1] <= expected[len(actual) - 1]
 
 
 def permutate(springs, expected):
@@ -35,14 +32,15 @@ def permutate(springs, expected):
 
 answer_a = 0
 answer_b = 0
-for line in lines:
-    # print('.', end='', flush=True)
+for i, line in enumerate(lines):
+    print(i)
     springs, counts = line.split(' ')
     counts = [int(count) for count in counts.split(',')]
     answer_a += len(list(permutate(springs, counts)))
-    # answer_b += len([config for config in permutate('?'.join([springs] * 5), counts * 5)])
+    for _ in permutate('?'.join([springs] * 5), counts * 5):
+        answer_b += 1
 
 print()
 print(answer_a)
 print(answer_b)
-# submit(answer_b)
+submit(answer_b)
